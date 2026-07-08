e217fbdac57808f8ce0c6c09b949248f5c1b3b11
Nghia Pham <ngfamgithub@gmail.com>
2026-07-08 09:49:55 +0700

    directory setup

---
+++ src/LiquidationRouter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity 0.8.28;
+
+import {AaveAdapter} from "vault-contracts/applications/aave/AaveAdapter.sol";
+
+contract LiquidationRouter {
+
+}
